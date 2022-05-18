7a773e74a7cc0859e654d2ce7f4a02b6c2edba48
KalliLahari <85424505+KalliLahari@users.noreply.github.com>
2022-05-18 11:52:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function prelod(){
+
+}
+
+function setup(){
+    canvas=createCanvas(350,400);
+    canvas.center();
+}
+
+function draw(){
+    
+}
+
+function take_pic(){
+    save("My Pic.png");
+}
